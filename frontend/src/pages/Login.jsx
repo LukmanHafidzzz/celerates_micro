6257d67b4515e27dd83398bdc122df4324eed561
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2024-04-04 16:11:27 +0700

    Upload login/regis

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Library
+
+// css
+import '../assets/Login.css'
+import '../assets/Global.css'
+
+export default function Login() {
+    return (
+        <>
+
+        </>
+    )
+}
